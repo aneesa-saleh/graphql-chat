43bff9e2fe54c2b807ebdb3c1a4aef32f8457c04
Aneesa Saleh <aneesa.saleh@gmail.com>
2019-08-06 03:34:02 +0100

    Create resolvers

--- index.js
+++ index.js
@@ -50,3 +50,29 @@ const typeDefs = gql`
     createdAt: String!
   }
 `;
+
+const resolvers = {
+  Query: {
+    users: () => db.users,
+    user: (root, { id }) => db.users.find(user => user.id === id),
+    messages: () => db.messages,
+  },
+
+  Mutation: {
+    addUser: (root, { email, name }) => {
+      const user = {
+        id: crypto.randomBytes(10).toString('hex'),
+        email,
+        name
+      }
+
+      db.users.push(user);
+
+      return user;
+    }
+  },
+
+  User: {
+    messages: user => db.messages.filter(message => message.userId === user.userId)
+  }
+}
